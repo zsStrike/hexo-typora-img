56c1bb9653236a0893f629c3cbfe273857b2b76d
zsStrike <2439758713@qq.com>
2020-10-21 13:36:54 +0800

    feat: add img path change function

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// match markdown image and covert to asset_img 
+hexo.extend.filter.register('before_post_render', function(data){
+
+    data.content = data.content.replace(/!{1}\[([^\[\]]*)\]\((\S*)\s?(?:".*")?\)/g,
+        function(match_str, label, path){
+
+            // if only one /
+            if( (path.split("/")).length == 2){
+                console.debug("Markdown Image Path: " + match_str);
+                console.debug(`Changed path: ![${label}](${path.split("/")[1]})`);
+                return `![${label}](${path.split("/")[1]})`;
+            }else{
+                console.debug("Markdown Image Path does not exists!");
+                return match_str;
+            }
+
+        });
+
+    return data;
+});
