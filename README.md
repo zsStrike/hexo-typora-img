877a4940efef20c7b21fcb3c7caa983402a320f9
zsStrike <2439758713@qq.com>
2022-05-04 16:39:03 +0800

    fix #1: add img src parse

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # hexo-typora-img
+
 Change typora img link to hexo img link. Now you can see the img both in typora editor and hexo server.
 
-在typora中编写博客，同时通过hexo发布文章。这样的搭配下，可能会导致在编写的时候能够看到图片，但是在发布的时候不能看到图片。
+在 Typora 中编写博客，同时通过 hexo 发布文章。在这样的搭配下，可能会导致在使用 Typora 编写的时候能够看到图片，但是在发布的时候由于没有正确处理图片路径而导致不能看到图片。
+
 `hexo-typora-img`就是为了解决这个问题产生的。
-
-
 
 ## 安装
 
@@ -12,6 +12,7 @@ Change typora img link to hexo img link. Now you can see the img both in typora 
 npm i hexo-typora-img
 ```
 
-具体配置参见这篇[博客](https://blog.zsstrike.top/2020/10/21/hexo%E5%92%8Ctypora%E6%90%AD%E9%85%8D%E5%86%99%E5%8D%9A%E5%AE%A2/)。
+具体配置参见这篇[博客](https://zsstrike.github.io/2020/10/21/hexo%E5%92%8Ctypora%E6%90%AD%E9%85%8D%E5%86%99%E5%8D%9A%E5%AE%A2/)。
+
 
 
--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 'use strict';
 
 // match markdown image and covert to hexo valid image path
-hexo.extend.filter.register('before_post_render', function(data){
-
+hexo.extend.filter.register('before_post_render', function (data) {
+    // typora markdown path: ![label](folder/filename.png) -> ![label](filename.png)
     data.content = data.content.replace(/!{1}\[([^\[\]]*)\]\((\S*)\s?(?:".*")?\)/g,
-        function(match_str, label, path){
-
-            // if only one /
-            if( (path.split("/")).length == 2){
-                console.debug("Markdown Image Path: " + match_str);
-                console.debug(`Changed path: ![${label}](${path.split("/")[1]})`);
+        function (match_str, label, path) {
+            if ((path.split("/")).length == 2) {
                 return `![${label}](${path.split("/")[1]})`;
-            }else{
-                console.debug("Markdown Image Path does not exists!");
+            } else {
+                console.error(`unresolved markdown img tag: ${path}`);
                 return match_str;
             }
 
         });
-
+    // typora img path: <img src="folder/filename.png" zoom="33%"/>
+    data.content = data.content.replace(/src="(.*?)"/g,
+        function (match_str, path) {
+            if (path.split("/").length == 2) {
+                return `src="${path.split('/')[1]}"`;
+            } else {
+                console.error(`unresolved img path: ${path}`);
+                return match_str;
+            }
+        });
     return data;
 });
